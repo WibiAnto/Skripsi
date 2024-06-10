bd3143604596964ba36126b67b667be2b6ea05a3
WibiAnto <anto.wibi03@gmail.com>
2024-06-10 11:21:45 +0700

    Update 10/06/2024 11:00(GMT +7): Remove unused files

--- Program/Scrapping Data.py
+++
@@ -1,9 +0,0 @@
-#Make Root Directory
-import bing_image_downloader.downloader as tools
-root_dir = 'Data'
-
-
-#Make Categorical Directory
-jenis = ["Monstera Adansonii", "Monstera Borsigiana", "Monstera Deliciosa Variegata"]
-for item in jenis:
-    tools.download(item, limit = 100, output_dir = root_dir, adult_filter_off = True, filter = "photo")
